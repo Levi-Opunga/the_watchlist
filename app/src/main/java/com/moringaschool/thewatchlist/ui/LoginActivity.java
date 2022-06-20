80c6996711fde8ecf3905b0ba2a2aaf679dc0968
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-20 21:45:03 +0300

    This will add the ui package

---
+++ app/src/main/java/com/moringaschool/thewatchlist/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.thewatchlist.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.thewatchlist.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
