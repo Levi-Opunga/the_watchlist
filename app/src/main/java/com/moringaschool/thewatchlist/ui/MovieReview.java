915a3fb4bdfe936783c60ee9a3831197d691443b
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-21 20:29:29 +0300

    This will add the firebase codes to save movie objects

--- app/src/main/java/com/moringaschool/thewatchlist/ui/MovieReview.java
+++ app/src/main/java/com/moringaschool/thewatchlist/ui/MovieReview.java
@@ -6,6 +6,8 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.thewatchlist.Constants;
 import com.moringaschool.thewatchlist.R;
 import com.moringaschool.thewatchlist.adapters.MoviePagerAdapter;
@@ -20,8 +22,16 @@ public class MovieReview extends AppCompatActivity {
 @BindView(R.id.viewpager)
     ViewPager2 viewpager;
     List<Result> list;
+
+    private DatabaseReference mSearchedMovie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        mSearchedMovie = FirebaseDatabase
+//                .getInstance()
+//                        .getReference().child(Constants.FIREBASE_CHILD_SEARCHED_MOVIES);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_review);
         ButterKnife.bind(this);
