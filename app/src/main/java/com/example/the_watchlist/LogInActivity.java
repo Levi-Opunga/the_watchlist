3d7072e3cd27d71f774792bdbc1e2f9d212dae28
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-21 23:48:37 +0300

    add sign up and log in activity

---
+++ app/src/main/java/com/example/the_watchlist/LogInActivity.java
@@ -0,0 +1,61 @@
+package com.example.the_watchlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener{
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton google, apple;
+    Button register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        google = findViewById(R.id.fab_google);
+        apple = findViewById(R.id.fab_apple);
+        register = findViewById(R.id.register);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final  LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        google.setTranslationY(300);
+        apple.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+//        google.setAlpha(v);
+//        apple.setAlpha(v);
+//        tabLayout.setAlpha(v);
+
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+        apple.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    };
+
+}
--- app/src/main/java/com/example/the_watchlist/OnBoardingFragment3.java
+++ app/src/main/java/com/example/the_watchlist/OnBoardingFragment3.java
@@ -1,5 +1,6 @@
 package com.example.the_watchlist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,27 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class OnBoardingFragment3 extends Fragment {
+
+    FloatingActionButton fab;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_on_boarding3, container, false);
 
+        fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LogInActivity.class);
+                startActivity(intent);
+            }
+        });
         return root;
     }
 }
